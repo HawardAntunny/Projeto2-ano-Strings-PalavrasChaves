f216d71e2a8970ec75b255a69df540f2d441b72a
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2025-06-25 09:30:43 -0300

    ss

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave (){
-    const texto = document.querySelector('#entrada-de-texto').ariaValueMax;
+    const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palvrachave');
 
-    campoResultado.textContent = texto
+    campoResultado.textContent = texto;
 }
