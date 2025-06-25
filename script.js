28bf464728dee805bf0ec9e04dc9212713909639
Haward Antunny <127453907+HawardAntunny@users.noreply.github.com>
2025-06-25 09:29:10 -0300

    ss

--- script.js
+++ script.js
@@ -5,4 +5,6 @@ botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 function mostraPalavraChave (){
     const texto = document.querySelector('#entrada-de-texto').ariaValueMax;
     const campoResultado = document.querySelector('#resultado-palvrachave');
+
+    campoResultado.textContent = texto
 }
